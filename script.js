1c8166d7b1dba39a386f5cdf76e671c02c05e8dd
gafur55 <g_mammadov@u.pacific.edu>
2023-01-14 18:46:17 -0800

    recipe app done. Close button works. The meal details is fetched

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function getMealList(){
     let searchInputTxt = document.getElementById('search-input').value.trim();
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
     .then(response => response.json())
-    .then(data => {
+    .then(data => { 
         let html = "";
         if(data.meals){
             data.meals.forEach(meal => {
